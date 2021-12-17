c5e7633dcb5cb7a74796a56a30277753e2e1eef2
TJ Tahmassebi <tjtahmas@gmail.com>
2021-12-16 21:20:03 -0500

    Updates

---
+++ precache-manifest.e220e6caec9f7be96f11f88d37afc042.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2eb6b0244257100425803ba142099d5",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "06c168c35333a50200d4",
+    "url": "/React-Portfolio/static/css/main.5455edf7.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c168c35333a50200d4",
+    "url": "/React-Portfolio/static/js/main.8499641f.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "37229c13cfb27f8f618ca57ed8b3af44",
+    "url": "/React-Portfolio/static/media/Note Taker.37229c13.png"
+  },
+  {
+    "revision": "f1ab61ddd42120382d203ba07fdff129",
+    "url": "/React-Portfolio/static/media/Round Table.f1ab61dd.png"
+  },
+  {
+    "revision": "fe6de3728568c5e5ef3b4691de6bb7f3",
+    "url": "/React-Portfolio/static/media/Spotiweather.fe6de372.png"
+  },
+  {
+    "revision": "f975cac0a6fc7b7ffe952cccc9305a2a",
+    "url": "/React-Portfolio/static/media/Tech Blog.f975cac0.png"
+  },
+  {
+    "revision": "6ba12cdddcb47633480fbefabca7c12a",
+    "url": "/React-Portfolio/static/media/Workout Tracker.6ba12cdd.png"
+  },
+  {
+    "revision": "4912dfc6c19d35b4b962fec9f819895f",
+    "url": "/React-Portfolio/static/media/cover-image.4912dfc6.jpg"
+  }
+]);
